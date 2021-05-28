ed2cb637d6f590fad86cf2e5846c1d2110505461
Aarinmahajan <74724212+Aarinmahajan@users.noreply.github.com>
2021-05-28 11:08:22 +0530

    Add files via upload

---
+++ js/Word.js
@@ -0,0 +1,46 @@
+const accessWord1 = "BRYAN LARA";
+const accessWord2 = "ROHIT SHARMA";
+const accessWord3 = "ANIL KUMBLE";
+const accessWord4 = "YUVRAJ SINGH";
+const accessWord5 = "SACHIN TENDULKAR";
+const accessWord6 = "FRED SPOFFORTH";
+
+function clues() {
+
+    //1
+    fill("black")
+    textSize(15)
+    text("W B U Y R N A R U I E I L N W Q R A A", 100, 60)
+    fill("black")
+    text("only batsman to score 421 runs in a test match(all capital letters please)", 100, 80)
+    //2
+    fill("black")
+    textSize(15)
+    text("T I S H E R P O  B S E R M N A H R W A", 100, 160)
+    fill("black")
+    text("highest score of 264 against sri lanka in odi(all capital letters please)", 100, 180)
+    //3
+    fill("black")
+    textSize(15)
+    text("W A U N U L I N H S D K E M B E U L W R", 100, 260) 
+    fill("black")
+    text("first person to take all 10 wickets in one innings(all capital letters please)", 100, 280)
+    //4
+    fill("black")
+    textSize(15)
+    text("I U V E A R A J Y D S N K H E G I N A Y O", 700, 60)
+    fill("black")
+    text("first person to hit 6 sixes in a row(all capital letters please)", 700, 80)
+    //5
+    fill("black")
+    textSize(15)
+    text("C A E S H N K I H E N D T E L K Y R A U L", 700, 160)
+    fill("black")
+    text("who is known as the god of cricket(all capital letters please)", 700, 180)
+    //6
+    fill("black")
+    textSize(15)
+    text("E D R F U S O P R H T F F O I D W J H Q P V", 700, 260)
+    fill("black")
+    text("first person to take a hat-trick in cricket(all capital letters please)", 700, 280)
+}
